a4fc4fdd3b4320bd14e407ed3e6a221354cb02cb
forafish <fora.luo@gmail.com>
2018-02-27 15:24:56 +0800

    update user-metric hooks

--- src/services/user-metric/user-metric-hooks.js
+++ src/services/user-metric/user-metric-hooks.js
@@ -1,4 +1,6 @@
+import { iff, isProvider, discard } from 'feathers-hooks-common';
 import { hooks as auth } from 'feathers-authentication';
+import { associateCurrentUser, queryWithCurrentUser } from 'feathers-authentication-hooks';
 import { hooks } from 'mostly-feathers-mongoose';
 import UserMetricEntity from '~/entities/metric-entity';
 
@@ -6,17 +8,33 @@ module.exports = function(options = {}) {
   return {
     before: {
       all: [
-        auth.authenticate('jwt')
+        auth.authenticate('jwt'),
+        iff(isProvider('external'),
+          queryWithCurrentUser({ idField: 'id', as: 'user' }))
       ],
-      get: [],
-      find: [],
-      create: [],
-      update: [],
-      patch: [],
-      remove: [],
+      find: [
+        hooks.prefixSelect('workout', { excepts: ['games', 'insights']})
+      ],
+      get: [
+        hooks.prefixSelect('workout', { excepts: ['games', 'insights']})
+      ],
+      create: [
+        iff(isProvider('external'),
+          associateCurrentUser({ idField: 'id', as: 'user' })),
+      ],
+      update: [
+        iff(isProvider('external'),
+          associateCurrentUser({ idField: 'id', as: 'user' })),
+      ],
+      patch: [
+        iff(isProvider('external'),
+          associateCurrentUser({ idField: 'id', as: 'user' })),
+      ]
     },
     after: {
       all: [
+        hooks.populate('metric', { path: '@type' }), // absolute path
+        hooks.populate('user', { service: 'users' }),
         hooks.presentEntity(UserMetricEntity, options),
         hooks.responder()
       ]
