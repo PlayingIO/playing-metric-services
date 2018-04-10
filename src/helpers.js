52b2ba486cc36fce916aa4af4b1a751d4352e781
forafish <fora.luo@gmail.com>
2018-04-10 15:56:45 +0800

    change createUserMetrics

--- src/helpers.js
+++ src/helpers.js
@@ -83,19 +83,17 @@ export const updateCompoundValues = (userCompounds, userScores) => {
   });
 };
 
-export const createUserMetrics = (app) => {
+export const createUserMetrics = async (app, user, rewards, variables) => {
   const svcUserMetrics = app.service('user-metrics');
-  return async (user, rewards, variables) => {
-    const create = fp.reduce((arr, reward) => {
-      if (reward.metric) {
-        reward.metric = helpers.getId(reward.metric);
-        reward.user = user;
-        reward.variables = variables;
-        return arr.concat(svcUserMetrics.create(reward));
-      }
-      return arr;
-    }, []);
-    const metrics = await Promise.all(create(rewards));
-    return fp.flatten(metrics);
-  };
+  const create = fp.reduce((arr, reward) => {
+    if (reward.metric) {
+      reward.metric = helpers.getId(reward.metric);
+      reward.user = user;
+      reward.variables = variables;
+      return arr.concat(svcUserMetrics.create(reward));
+    }
+    return arr;
+  }, []);
+  const metrics = await Promise.all(create(rewards));
+  return fp.flatten(metrics);
 };
