2677a7dd2b16b54f106b23deb69d44d41b821d48
forafish <fora.luo@gmail.com>
2018-04-26 19:12:38 +0800

    remove time log of esm import

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 require = require("esm")(module/*, options*/);
-console.time('playing-metric-services import');
 module.exports = require('./src/index').default;
 module.exports.entities = require('./src/entities').default;
 module.exports.models = require('./src/models').default;
 module.exports.helpers = require('./src/helpers');
-console.timeEnd('playing-metric-services import');
