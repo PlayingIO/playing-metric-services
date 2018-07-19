abb76f90f83fb3a984e34eede85e14cf4a180670
forafish <fora.luo@gmail.com>
2018-07-19 16:28:49 +0800

    lint empty lines

--- src/services/user-metric/user-metric.accepts.js
+++ src/services/user-metric/user-metric.accepts.js
@@ -9,7 +9,7 @@ module.exports = function accepts (context) {
   const value = { arg: 'value', type: 'string', required: true, description: 'Value of metric' };
   const variables = { arg: 'variables', type: 'object', default: {}, description: 'Variables of metric' };
   const verb = { arg: 'verb', type: 'string', required: true, description: 'Verb of metric' };
-  
+
   return {
     create: [ metric, user, verb, value, chance, variables ]
   };
